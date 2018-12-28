ba80f42d18d19aa4f02eb12fb8559cde846cb43a
deehgimenes <andrepapagimenes@gmail.com>
2018-12-28 17:20:12 -0200

    Refinamento de códigos

---
+++ Udemy/JavaScript - Curso Completo/Códigos em Aula/Anotações.js
@@ -0,0 +1,7 @@
+// Classe é um conjunto de atributos e métodos.
+// Comando interno THIS faz a refência a atributos e métodos da classe mencionada.
+// O Encapsulamento controla o acesso a um atributo ou método. (PUBLIC, PROTECTED e PRIVATE)
+// PUBLIC: 
+// PROTECTED: 
+// PRIVATE: Apenas atributos e métodos da mesma classe conseguem ter acesso a ele.
+// Métodos GETTERS e SETTERS permitem definir como acessar os valores.
