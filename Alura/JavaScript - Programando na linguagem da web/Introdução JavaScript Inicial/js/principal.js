8f87016f2c3f3f235d62a0562eb12568dea40a54
deehgimenes <andrepapagimenes@gmail.com>
2019-01-04 17:03:11 -0200

    A06 - Adicionando pacientes na tabela

--- Alura/JavaScript - Programando na linguagem da web/Introdução JavaScript Inicial/js/principal.js
+++ Alura/JavaScript - Programando na linguagem da web/Introdução JavaScript Inicial/js/principal.js
@@ -1,29 +1,66 @@
-var paciente = document.querySelector("#primeiro-paciente");
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "aChurch";
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+for(var i = 0; i < pacientes.length; i++){
 
-var tdImc = paciente.querySelector(".info-imc");
+    var paciente = pacientes[i];
 
-var pesoValido = true;
-var alturaValida = true;
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
 
-if(peso <= 0 || peso >= 1000){
-    console.log("Peso Inválido!");
-    pesoValido = false;
-    tdImc.textContent = "Peso Inválido";
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+
+    var tdImc = paciente.querySelector(".info-imc");
+
+    var pesoValido = true;
+    var alturaValida = true;
+    
+    if(peso <= 0 || peso >= 1000){
+        console.log("Peso Inválido!");
+        pesoValido = false;
+        tdImc.textContent = "Peso Inválido";
+        paciente.classList.add("peso-invalido");
+    }
+
+    if(altura <= 0 || altura >= 3.00){
+        console.log("Altura Inválida!");
+        alturaValida = false;
+        tdImc.textContent = "Altura Inválido";
+        paciente.classList.add("altura-invalida");
+    }
+
+    if(pesoValido && alturaValida){
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
 }
 
-if(altura <= 0 || altura >= 3.00){
-    console.log("Altura Inválida!");
-    alturaValida = false;
-    tdImc.textContent = "Altura Inválido";
-}
+/*---------------- FUNÇÃO ANÔNIMA ----------------
+titulo.addEventListener("click", function(){
+    console.log("Teste!")
+}); */
 
-if(pesoValido && alturaValida){
-    var imc = peso / (altura * altura);
-    tdImc.textContent = imc;
-}
+/*---------------- FUNÇÃO NOMEADA ----------------
+function mostraMensagem(){
+    console.log("Olá eu fui clicado");
+} */
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function(event){
+    event.preventDefault();
+
+    var form = document.querySelector("#form-adiciona");
+
+    var nome = form.nome;
+    var peso = form.peso;
+    var altura = form.altura;
+    var gordura = form.gordura;
+
+    console.log(altura);
+    console.log(nome);
+    console.log(peso);
+    console.log(gordura);
+});
